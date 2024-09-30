94621e0d1a4e5c97d2005644f2a698efc68644e0
ziani96 <81708016+ziani96@users.noreply.github.com>
2024-09-30 15:59:21 +0100

    find classes page layout inital format

--- src/components/AllClasses.jsx
+++ src/components/AllClasses.jsx
@@ -1,13 +1,68 @@
 import React from "react";
 import Header from "./Header.jsx";
 import "../stylesheets/AllClasses.css";
+import SingleEventCard from "./SingleEventCard.jsx";
+import Banner from "./Banner.jsx";
 
 const AllClasses = () => {
   return (
-    <div>
-      <Header />
-      <h1>All Classes</h1>
-    </div>
+    <>
+      <section>
+        <Header />
+        <Banner />
+        <h1 className="header">All Classes</h1>
+      </section>
+      {/* <div className="classes-nav">
+        <nav>
+          <ul>
+            <li>Spin</li>
+            <li>HIIT</li>
+            <li>Pilates</li>
+            <li>Strength Training</li>
+          </ul>
+        </nav>
+      </div> */}
+      <section className="classes">
+        <div className="filter-container">
+          <div class="filter-box">
+            <label for="category">Classes:</label>
+            <select id="category" name="category">
+              <option value="">All Class</option>
+              <option value="fitness">Fitness</option>
+              <option value="yoga">Yoga</option>
+              <option value="hiit">HIIT</option>
+            </select>
+          </div>
+
+          <div class="filter-box">
+            <label for="location">Trainer:</label>
+            <select id="location" name="location">
+              <option value="">All Trainers</option>
+              <option value="new-york">Joel</option>
+              <option value="london">Steve</option>
+              <option value="sydney">Sydney</option>
+            </select>
+          </div>
+        </div>
+        <div className="all-rows">
+          <div className="card-row">
+            <SingleEventCard />
+            <SingleEventCard />
+            <SingleEventCard />
+          </div>
+          <div className="card-row">
+            <SingleEventCard />
+            <SingleEventCard />
+            <SingleEventCard />
+          </div>
+          <div className="card-row">
+            <SingleEventCard />
+            <SingleEventCard />
+            <SingleEventCard />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
